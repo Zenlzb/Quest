3a036a4c9c22e37fb274c396c6fe799849d521b7
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-05-20 19:26:14 +0800

    Add Colors.js

---
+++ assets/themes/colors.js
@@ -0,0 +1,15 @@
+const palette = {
+    lightGreen: '#ADFF5F',
+    green: '#11FEB0',
+    pink: '#FF5864',
+    orange: '#FF5E37',
+    yellow: '#f5f376',
+}
+
+const colors = {
+    background: palette.yellow,
+    button1: palette.pink,
+    button2: palette.lightGreen,
+}
+
+export default colors
