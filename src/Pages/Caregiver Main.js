55c2ae7284f73dc2a0580f35385fb66ec47fb267
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-05-21 18:07:27 +0800

    Add Caregiver Main

---
+++ src/Pages/Caregiver Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const CaregiverMain = () => {
+    return(
+        <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Caregiver Main</Text>
+        </View>
+    )
+}
+
+export default CaregiverMain;
