86d30284f10ef7e051692e4d00c293e977b901d9
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-06-13 23:02:12 +0800

    Restructure CaregiverMain Components

--- src/Pages/CaregiverMain.js
+++ src/Pages/CaregiverMain.js
@@ -19,7 +19,7 @@ const CaregiverMain = () => {
 
     const handleSignOut = () => {setSignOutConfirm(true)}
     const handleAddChild = () => {
-        if (childNameInput === 'All' || childNameInput === 'all' || childNameInput === '') {
+        if (childNameInput === '') {
             setErrorCode('invalid')
             return
         }
@@ -50,11 +50,10 @@ const CaregiverMain = () => {
         const handleRemoveChild = () => {Children.deleteChild(userId, item)}
         return (
             <CustomButton
-                buttonStyle={[styles.button, {marginBottom: 8}]}
-                textStyle={{fontFamily: 'balsamiq', fontSize:10}}
+                buttonStyle={[styles.button, {marginBottom: 8, width: '100%', height: 70}]}
+                textStyle={{fontFamily: 'balsamiq', fontSize:15}}
                 onPress={handleRemoveChild}
             >{item.name}</CustomButton>
-
         )
     }
 
@@ -93,48 +92,61 @@ const CaregiverMain = () => {
                         )
                     }}
             />
-            <View style={{marginTop: 45, marginRight: 300, marginBottom: 8}}>
+            <View style={styles.topContainer}>
                 <CustomButton
                     buttonStyle={[styles.button, {marginLeft: 8}]}
-                    textStyle={[styles.text, {paddingVertical: 1}]}
+                    textStyle={[styles.text, {paddingVertical: 1, fontSize: 17}]}
                     onPress={handleSignOut}
                 >Sign Out</CustomButton>
+                <Text style={[styles.text, {marginRight: 8, fontSize: 17}]}>
+                    Welcome, Caregiver {auth.currentUser.displayName.length >= 15
+                    ? auth.currentUser.displayName.substr(0, 12) + '...' : auth.currentUser.displayName}
+                </Text>
             </View>
 
-            <View style={{position: 'absolute', marginTop: 45}}>
-                <Text style={styles.text}>Caregiver {auth.currentUser.displayName}</Text>
+            <View style={{flexDirection: 'row', width: '100%', height: 70, paddingHorizontal: 8, justifyContent: 'space-between'}}>
+                <Text style={[styles.text, {fontSize: 40}]}>Children</Text>
+                <View style={{flexDirection: 'row', marginTop: 21, height: 25}}>
+                    <CustomButton
+                        buttonStyle={[styles.button, {marginRight: 8}]}
+                        textStyle={{fontFamily: 'balsamiq', fontSize: 15, color: colors.button3}}
+                        onPress={() => {}}
+                    >Rewards</CustomButton>
+                    <CustomButton
+                        buttonStyle={styles.button}
+                        textStyle={{fontFamily: 'balsamiq', fontSize: 15}}
+                        onPress={handleCreateQuest}
+                    >+ Create Quest</CustomButton>
+                </View>
             </View>
 
-            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                <TextInput
-                    style={[styles.textInput, {marginRight: 8}]}
-                    placeholder={"Add Child"}
-                    onChangeText={handleChildNameUpdate}
-                    value={childNameInput}
+            <View style={{width: '95%', height: '79%', borderWidth: 2, padding: 5, borderRadius: 10, alignItems: 'center'}}>
+                <View style={{width: '90%', height: 20, flexDirection: 'row', alignItems: 'center', marginVertical: 8}}>
+                    <TextInput
+                        style={[styles.textInput, {marginRight: 8}]}
+                        placeholder={"Add Child"}
+                        onChangeText={handleChildNameUpdate}
+                        value={childNameInput}
+                    />
+                    <CustomButton
+                        buttonStyle={[styles.button, {height: 33}]}
+                        textStyle={{fontFamily: 'balsamiq'}}
+                        onPress={handleAddChild}
+                    >Add</CustomButton>
+                </View>
+                <ErrorText
+                    errorCode={errorCode}
+                    setErrorCode={setErrorCode}
+                    errors={errors}
                 />
-                <CustomButton
-                    buttonStyle={styles.button}
-                    textStyle={{fontFamily: 'balsamiq'}}
-                    onPress={handleAddChild}
-                >Add</CustomButton>
-            </View>
-            <ErrorText
-                errorCode={errorCode}
-                setErrorCode={setErrorCode}
-                errors={errors}
-            />
-            <CustomButton
-                buttonStyle={[styles.button, {marginTop: 8}]}
-                textStyle={{fontFamily: 'balsamiq'}}
-                onPress={handleCreateQuest}
-            >+ Create Quest</CustomButton>
-            <Text style={{marginTop:8, fontFamily: 'balsamiq'}}>List of Children</Text>
-            <View style={{marginTop:8, height:100, width: 100, alignItems: 'center'}}>
+
                 <FlatList
+                    style={{width: '90%', height: '70%', marginTop:8}}
                     data={childList}
                     renderItem={renderItem}
                     keyExtractor={item => item.key}
                 />
+
             </View>
         </View>
     )
@@ -145,7 +157,16 @@ const styles = StyleSheet.create({
         flex:1,
         flexDirection: 'column',
         backgroundColor: colors.background,
-        alignItems: 'center'
+        alignItems: 'center',
+        width: '100%'
+    },
+    topContainer: {
+        flexDirection: 'row',
+        marginTop: 45,
+        marginBottom: 8,
+        justifyContent: 'space-between',
+        height: 27,
+        width: '100%'
     },
     text: {
         fontSize: 20,
@@ -155,10 +176,11 @@ const styles = StyleSheet.create({
     textInput: {
         borderWidth: 1,
         borderColor: 'black',
+        borderRadius: 5,
         backgroundColor: '#fff',
         fontSize: 20,
-        width: 200,
-        height: 30,
+        width: '80%',
+        height: 33,
         paddingLeft: 5,
         fontFamily: 'balsamiq'
     },
