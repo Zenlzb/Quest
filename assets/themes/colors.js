6a0a24244894a12461e879b5e59bf7307eb78b3f
Hanif Kamal <hanif.kamal47@gmail.com>
2021-06-17 21:39:40 +0800

    Add purple and changed orange

--- assets/themes/colors.js
+++ assets/themes/colors.js
@@ -3,15 +3,17 @@ const palette = {
     green: '#11FEB0',
     blue: '#71bed1',
     pink: '#FF5864',
-    orange: '#FF5E37',
+    orange: '#F79940',
     yellow: '#f5f376',
     gold: '#FFD700',
     transparentBlack: '#000000AA',
+    purple: '#F29DDE'
 }
 
 const colors = {
     background: palette.yellow,
     background2: palette.blue,
+    background3: palette.purple,
     transparentBackground: palette.transparentBlack,
     button1: palette.pink,
     button2: palette.green,
