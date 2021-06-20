9d718644db62c669e0bf79c220db42680bbe1048
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-06-20 16:57:15 +0800

    Add Pending and History of Reward Claims

--- src/Components/RewardHistory.js
+++ src/Components/RewardHistory.js
@@ -1,26 +1,39 @@
 import React, {useEffect, useState} from 'react'
-import {FlatList, Modal, StyleSheet, Text, View} from 'react-native';
+import {FlatList, Modal, ScrollView, StyleSheet, Text, View} from 'react-native';
 import colors from "../../assets/themes/colors";
 import CustomButton from "./Button";
 import * as Rewards from '../../api/rewards'
 import RewardListItem from "./RewardListItem";
+import CustomPopup from "./Popup";
 
 const RewardHistoryModal = (props) => {
     const [pendingClaimList, setPendingClaimList] = useState([])
+    const [completeClaimList, setCompleteClaimList] = useState([])
+    const [completeClaimPopup, setCompleteClaimPopup] = useState(false)
+    const [completeClaimId, setCompleteClaimId] = useState('')
+    const [completeClaimName, setCompleteClaimName] = useState('')
 
     useEffect(() => {
-        return Rewards.pendingClaimListSubscribe(props.userId, setPendingClaimList)
+        return Rewards.claimListSubscribe(props.userId, setPendingClaimList, true)
+    }, [])
+
+    useEffect(() => {
+        return Rewards.claimListSubscribe(props.userId, setCompleteClaimList, false)
     }, [])
 
     const test = () => {
-        Rewards.createRewardClaim(props.userId,  3, 'Bob', 'Book', 100, new Date().toJSON())
+        Rewards.createRewardClaim(props.userId,  3, 'Bob', 'AAAAAAAAAAAAAAAAAAAAAAAAAA', 99999999999999, new Date().toJSON())
     }
     const claimItem = ({ item }) => {
         return (
             <RewardListItem
                 item={item}
                 mode='caregiverRewardHistory'
-                onPress={() => {}}
+                onPress={() => {
+                    setCompleteClaimId(item.id)
+                    setCompleteClaimName(item.rewardName)
+                    setCompleteClaimPopup(true)
+                }}
             />
         )
     }
@@ -33,21 +46,69 @@ const RewardHistoryModal = (props) => {
             }}
         >
             <View style={styles.container}>
+                <CustomPopup
+                    visibility={completeClaimPopup}
+                    titleText={'Complete Claim'}
+                    bodyText={`Complete: '${completeClaimName}' ?`}
+                    containerStyle={{backgroundColor: colors.background}}
+                    textStyle={{color:'black'}}
+                    buttonList={() => {
+                        return (
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between', width: '80%'}}>
+                                <CustomButton
+                                    buttonStyle={[styles.button, {backgroundColor: colors.button2}]}
+                                    textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
+                                    onPress={() => {
+                                        Rewards.completeRewardClaim(props.userId, completeClaimId)
+                                        setCompleteClaimPopup(false)
+                                    }}
+                                >Accept</CustomButton>
+                                <CustomButton
+                                    buttonStyle={styles.button}
+                                    textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
+                                    onPress={() => {
+                                        Rewards.denyRewardClaim(props.userId, completeClaimId)
+                                        setCompleteClaimPopup(false)
+                                    }}
+                                >Deny</CustomButton>
+                                <CustomButton
+                                    buttonStyle={styles.button}
+                                    textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
+                                    onPress={() => {setCompleteClaimPopup(false)}}
+                                >Cancel</CustomButton>
+                            </View>
+                        )
+                    }}
+                />
                 <View style={styles.popupContainer}>
-                    <View style={{alignItems: 'flex-start', width: '100%', marginBottom: 5}}>
-                        <Text style={[styles.text, {fontSize: 30}]}>Pending</Text>
-                    </View>
-                    <View style={{width: '100%', height: 200, borderWidth: 2, borderRadius: 5, marginBottom: 8, paddingHorizontal: 8}}>
-                        <View style={{width: '100%', justifyContent: 'center', marginLeft: 7}}>
-                            <FlatList
-                                data={pendingClaimList}
-                                renderItem={claimItem}
-                                keyExtractor={item => item.id}
-                            />
+                    <ScrollView style={{height: '95%', width: '100%', marginBottom: 8}}>
+                        <View style={{alignItems: 'flex-start', width: '100%', marginBottom: 5}}>
+                            <Text style={[styles.text, {fontSize: 30}]}>Pending</Text>
                         </View>
+                        <View style={{width: '100%', borderWidth: 2, borderRadius: 5, marginBottom: 8, paddingHorizontal: 8, paddingBottom: 8}}>
+                            <View style={{width: '100%', justifyContent: 'center', marginLeft: 7}}>
+                                <FlatList
+                                    data={pendingClaimList}
+                                    renderItem={claimItem}
+                                    keyExtractor={item => item.id}
+                                />
+                            </View>
 
-                    </View>
+                        </View>
+                        <View style={{alignItems: 'flex-start', width: '100%', marginBottom: 5}}>
+                            <Text style={[styles.text, {fontSize: 30}]}>History</Text>
+                        </View>
+                        <View style={{width: '100%', borderWidth: 2, borderRadius: 5, marginBottom: 8, paddingHorizontal: 8, paddingBottom: 8}}>
+                            <View style={{width: '100%', justifyContent: 'center', marginLeft: 7}}>
+                                <FlatList
+                                    data={completeClaimList}
+                                    renderItem={claimItem}
+                                    keyExtractor={item => item.id}
+                                />
+                            </View>
 
+                        </View>
+                    </ScrollView>
                     <View style={{flexDirection: 'row', marginBottom: 8}}>
                         <CustomButton
                             buttonStyle={styles.button}
@@ -59,7 +120,7 @@ const RewardHistoryModal = (props) => {
                         buttonStyle={styles.button}
                         textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
                         onPress={() => props.toggleVisibility(false)}
-                    >Cancel</CustomButton>
+                    >Close</CustomButton>
                 </View>
             </View>
         </Modal>
--- src/Components/RewardListItem.js
+++ src/Components/RewardListItem.js
@@ -6,16 +6,29 @@ import CoinIcon from "./CoinIcon";
 const RewardListItem = (props) => {
     if (props.mode === 'caregiverReward') {
         const { item, onPress } = props
+        const AvailableText = () => {
+            if (item.availability) {
+                return (
+                    <View style = {{backgroundColor: colors.button2, borderRadius: 5, paddingHorizontal: 5, justifyContent: 'center', height: 25}}>
+                        <Text style={[styles.text, {color: 'white', fontSize: 15}]}>Available</Text>
+                    </View>
+                )
+            } else {
+                return (
+                    <View style = {{backgroundColor: colors.button1, borderRadius: 5, paddingHorizontal: 5, justifyContent: 'center', height: 25}}>
+                        <Text style={[styles.text, {color: 'white', fontSize: 15}]}>Not Available</Text>
+                    </View>
+                )
+            }
+        }
         return (
             <Pressable
                 style={styles.container}
                 onPress={onPress}
             >
                 <Text style={styles.text} numberOfLines={1}>{item.name}</Text>
-                <View style={styles.textContainer}>
-                    {item.availability
-                        ? <Text style={[styles.text, {color: colors.button2}]}>Available</Text>
-                        : <Text style={[styles.text, {color: colors.button1}]}>Not Available</Text>}
+                <View style={[styles.textContainer, {height: 30, alignItems: 'flex-end'}]}>
+                    <AvailableText/>
                     <View style={{flexDirection: 'row'}}>
                         <Text style={[styles.text, {width: 80, textAlign: 'right'}]} numberOfLines={1}>{item.cost}</Text>
                         <CoinIcon style={{marginLeft: 2, marginTop: 5}} dimension={25}/>
@@ -40,7 +53,10 @@ const RewardListItem = (props) => {
                 onPress={onPress}
             >
                 <View style={styles.textContainer}>
-                    <Text style={[styles.text, {fontSize: 18}]} numberOfLines={1}>{item.rewardName} x{item.quantity}</Text>
+                    <View style={{flexDirection: 'row', width: '60%'}}>
+                        <Text style={[styles.text, {fontSize: 18, width: '80%', textAlign: 'left'}]} numberOfLines={1}>{item.rewardName}</Text>
+                        <Text style={[styles.text, {fontSize: 18, width: '20%', textAlign: 'left'}]} numberOfLines={1}> x{item.quantity}</Text>
+                    </View>
                     <View style={{flexDirection: 'row'}}>
                         <Text style={[styles.text, {width: 80, textAlign: 'right', fontSize: 18}]} numberOfLines={1}>{item.rewardCost * item.quantity}</Text>
                         <CoinIcon style={{marginLeft: 2, marginTop: 3}} dimension={20}/>
@@ -74,7 +90,7 @@ const styles = StyleSheet.create({
     textContainer: {
         flexDirection: 'row',
         width: '100%',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
     },
     text: {
         fontSize: 22,
