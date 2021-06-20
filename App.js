b85bfa9ace12db3949d8da2399a053e14ff783dd
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-06-20 16:44:27 +0800

    Ignore Warning

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AppLoading from 'expo-app-loading';
 import AuthHandler from "./src/Navigation/AuthHandler";
 import { LogBox } from 'react-native';
 LogBox.ignoreLogs(['Setting a timer']);
+LogBox.ignoreLogs(['VirtualizedLists should never be nested inside plain ScrollViews with the same orientation - use another VirtualizedList-backed container instead.']);
 
 export default function App() {
   let [fontsLoaded] = useFonts({
