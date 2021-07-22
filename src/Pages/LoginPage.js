d35bddf2328f06ecbb254e9c7e6dca7edfeb2a13
Lee Zen Been <leezenbeen@yahoo.com.sg>
2021-07-22 17:18:00 +0800

    Add New User Help popup

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Keyboard, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Keyboard, Modal, Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
 import CustomButton from "../Components/Button";
 import colors from "../../assets/themes/colors";
 import {signIn} from "../../api/auth";
@@ -9,6 +9,7 @@ const LoginPage = ({ navigation }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [childLink, setChildLink] = useState('')
+    const [loginHelp, setLoginHelp] = useState(false)
 
     const [errorCode, setErrorCode] = useState(null)
     const errors = [
@@ -37,46 +38,79 @@ const LoginPage = ({ navigation }) => {
     return(
 
         <View style={styles.container}>
-        <Text style={styles.titleText}>Quest</Text>
-        <View style={styles.loginContainer}>
-            <View style={{flexDirection: 'column', alignItems: 'center'}}>
-                <TextInput
-                    style={[styles.textInput, {marginBottom: 8}]}
-                    placeholder={"Email"}
-                    keyboardType='email-address'
-                    onChangeText={handleEmailUpdate}
-                    value={email}
-                />
-                <TextInput
-                    style={[styles.textInput, {marginBottom: 8}]}
-                    placeholder={"Password"}
-                    onChangeText={handlePasswordUpdate}
-                    value={password}
-                    secureTextEntry
-                />
-                <TextInput
-                    style={styles.textInput}
-                    placeholder={"Link Child Account"}
-                    onChangeText={handleChildLinkUpdate}
-                    value={childLink}
-                />
+            <Modal
+                transparent={true}
+                visible={loginHelp}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={[styles.modalPopupContainer, {justifyContent: 'space-between'}]}>
+                        <View style={{width: '100%', height: '80%', alignItems: 'flex-start'}}>
+                            <Text style={[styles.text, {fontSize: 21}]}>1) Setting up a Caregiver Account</Text>
+                            <Text style={[styles.text, {fontSize: 13}]}>Tap Create Caregiver Account on the login page{'\n'}</Text>
+                            <Text style={[styles.text, {fontSize: 19}]}>2) Logging into a Caregiver Account</Text>
+                            <Text style={[styles.text, {fontSize: 13, textAlign: 'left'}]}>Enter your login credentials, leaving 'Link Child Account' blank{'\n'}</Text>
+                            <Text style={[styles.text, {fontSize: 21}]}>3) Setting up a Child Account</Text>
+                            <Text style={[styles.text, {fontSize: 13, textAlign: 'left'}]}>Once you enter the caregiver account, there will be an option to Add Child.</Text>
+                            <Text style={[styles.text, {fontSize: 13, textAlign: 'left'}]}>After you have added a child, log in on your child's mobile device by inputting your login credentials, and your child's name in 'Link Child Account'</Text>
+                        </View>
+
+                        <View style={{width: '100%', alignItems: 'center'}}>
+                            <CustomButton
+                                buttonStyle={{backgroundColor: colors.button1, justifyContent: 'center',}}
+                                textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
+                                onPress={() => setLoginHelp(false)}
+                            >Close</CustomButton>
+                        </View>
+
+                    </View>
+                </View>
+
+            </Modal>
+            <Text style={styles.titleText}>Quest</Text>
+            <View style={styles.loginContainer}>
+                <View style={{flexDirection: 'column', alignItems: 'center'}}>
+                    <TextInput
+                        style={[styles.textInput, {marginBottom: 8}]}
+                        placeholder={"Email"}
+                        keyboardType='email-address'
+                        onChangeText={handleEmailUpdate}
+                        value={email}
+                    />
+                    <TextInput
+                        style={[styles.textInput, {marginBottom: 8}]}
+                        placeholder={"Password"}
+                        onChangeText={handlePasswordUpdate}
+                        value={password}
+                        secureTextEntry
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder={"Link Child Account"}
+                        onChangeText={handleChildLinkUpdate}
+                        value={childLink}
+                    />
+                </View>
+                <CustomButton
+                    buttonStyle={styles.submitButton}
+                    textStyle={{fontFamily: 'balsamiq'}}
+                    onPress={handleLogin}
+                >Log-in</CustomButton>
             </View>
-            <CustomButton
-                buttonStyle={styles.submitButton}
-                textStyle={{fontFamily: 'balsamiq'}}
-                onPress={handleLogin}
-            >Log-in</CustomButton>
-        </View>
             <ErrorText
                 errorCode={errorCode}
                 setErrorCode={setErrorCode}
                 errors={errors}
             />
-        <CustomButton
-            buttonStyle={styles.createAccountButton}
-            textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
-            onPress={() => navigation.navigate("Create Account")}
-        >Create Caregiver Account</CustomButton>
+            <CustomButton
+                buttonStyle={styles.createAccountButton}
+                textStyle={{fontSize:15, fontFamily: 'balsamiq'}}
+                onPress={() => navigation.navigate("Create Account")}
+            >Create Caregiver Account</CustomButton>
+            <Pressable onPress={() => setLoginHelp(true)}>
+                <Text style={{fontFamily: 'balsamiq', marginTop: 10}}>New User?</Text>
+            </Pressable>
+
+
 
     </View>
     )
@@ -116,7 +150,27 @@ const styles = StyleSheet.create({
     },
     loginContainer: {
         flexDirection: 'row',
-    }
+    },
+    modalContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.transparentBackground,
+    },
+    modalPopupContainer: {
+        backgroundColor: colors.background,
+        padding:10,
+        borderRadius: 5,
+        width: 350,
+        height: '60%',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontFamily: 'balsamiq',
+        textAlign: 'center'
+    },
 });
 
 export default LoginPage
